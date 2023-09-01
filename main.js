fb6833dbaf5218bea6a1cd86cd099dad444e1da4
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-09-01 08:53:31 -0300

    Adicionado recursos para não duplicar imagens no carrinho, decremento e incremento

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Códigos - Aula01 => JS2023 - Aula02 => CSS2908 - Aula03 => MAGAZINEHASH
+// Códigos - Aula01 => JS2023 - Aula02 => CSS2908 - Aula03 => MAGAZINEHASH - Aula04 => INTENSIVOJS15
 import { renderizarCatalogo } from "./src/cartaoProduto";
 import { inicializarCarrinho } from "./src/menuCarrinho";
 
