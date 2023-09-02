9b5a77fafe64a8998d3a7a41bd58fa360e803351
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-09-02 07:35:34 -0300

    Adicionado checkout e histórico de pedidos armazenando no localstorage

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-// Códigos - Aula01 => JS2023 - Aula02 => CSS2908 - Aula03 => MAGAZINEHASH - Aula04 => INTENSIVOJS15
+// Códigos - Aula01 => JS2023 - Aula02 => CSS2908 - Aula03 => MAGAZINEHASH - Aula04 => INTENSIVOJS15 - Aula05 => GITHUB10
 import { renderizarCatalogo } from "./src/cartaoProduto";
-import { inicializarCarrinho } from "./src/menuCarrinho";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
+import {
+  atualizarPrecoCarrinho,
+  inicializarCarrinho,
+  renderizarProdutosCarrinho,
+} from "./src/menuCarrinho";
 
 renderizarCatalogo();
 inicializarCarrinho();
+renderizarProdutosCarrinho();
+atualizarPrecoCarrinho();
+inicializarFiltros();
+inicializarFiltros;
